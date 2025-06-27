57adda7ae010804a58f28c1a9a69344ee960c8c6
samya abbas <sumoabbas@gmail.com>
2025-06-27 12:41:05 +0300

    "Added a alx_book_store and MySQLServer"

---
+++ MySQLServer.py
@@ -0,0 +1,33 @@
+import mysql.connector
+from mysql.connector import Error
+
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASS = "521996RRrr##"
+TARGET_DB = "alx_book_store"
+
+def main():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASS
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            cursor.execute(f"CREATE DATABASE IF NOT EXISTS {TARGET_DB};")
+            connection.commit()
+
+            print(f"Database '{TARGET_DB}' created successfully!")
+    except Error as err:
+        print(f"Error: {err}")
+    finally:
+        if connection and connection.is_connected():
+            cursor.close()
+            connection.close()
+
+if __name__ == "__main__":
+    main()
